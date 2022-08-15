96ffa7c542ef84698f34847f28fbf820ded51f7d
Kens Learning Curve <kenslearningcurve@gmail.com>
2022-08-15 14:14:29 +0200

    Initial commit

---
+++ ExampleClass/Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace ExampleClass.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Rating { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
